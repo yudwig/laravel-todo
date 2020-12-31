c8d27953768c91b1ae5486db138af96f689fd7a3
yudwig <yudwig.dev@gmail.com>
2020-12-31 16:49:18 +0900

    Add prototype views.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,34 @@
 @extends('app')
 
 @section('content')
-    <div class="title">
-        test
-    </div>
+    <a href="{{ route('categories.showEdit', ['id' => 1]) }}">カテゴリ1</a>
+    <hr class="border-t-1 border-gray-400 my-3">
+    <table class="mb-10">
+        <colgroup class="w-96"></colgroup>
+        <tr>
+            <td>
+                <a href="{{ route('tasks.showEdit', ['id' => 1]) }}">タスク1</a>
+            </td>
+            <td>
+                <form method="post" action="{{ route('tasks.update', ['id' => 1]) }}">
+                    @csrf
+                    <input type="hidden" name="completed" value="1">
+                    <a href="javascript:void(0);" onclick="this.closest('form').submit();">完了</a>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td><a>タスク2</a></td>
+            <td><a>完了</a></td>
+        </tr>
+        <tr>
+            <td><a class="line-through">タスク3</a></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><a href="{{ route('tasks.showCreate', ['id' => 1]) }}">追加</a></td>
+        </tr>
+    </table>
+    <a href="{{ route('categories.showCreate') }}">追加</a>
 @endsection
