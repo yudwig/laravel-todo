d69a6ef6f9e7d2bfe2df6c3fe3acf950604f75c6
yudwig <yudwig.dev@gmail.com>
2021-01-06 18:36:28 +0900

    Add factories and seeders.

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(2),
+            'completed' => $this->faker->numberBetween(0, 1),
+            'category_id' => Category::all()->random()
+        ];
+    }
+}
