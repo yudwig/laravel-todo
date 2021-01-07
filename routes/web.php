108da563534908de988f22d093e92b2e6779b43c
yudwig <yudwig.dev@gmail.com>
2021-01-07 14:54:39 +0900

    Modify routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,17 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/welcome', function() { return view('welcome'); });
-
-Route::get('/', function() { return view('tasks.index'); })->name('tasks.showIndex');
-Route::get('/categories/create', function() { return view('categories.create'); })->name('categories.showCreate');
-Route::get('/categories/{id}/edit', function() { return view('categories.edit'); })->name('categories.showEdit');
-Route::post('/categories/create', function() { echo "カテゴリー作成"; })->name('categories.create');
-Route::post('/categories/{id}/update', function($id) { echo "カテゴリー更新 (id: {$id})"; })->name('categories.update');
-Route::post('/categories/{id}/delete', function($id) { echo "カテゴリー削除 (id: {$id})"; })->name('categories.delete');
-Route::get('/categories/{id}/tasks/create', function() { return view('tasks.create'); })->name('tasks.showCreate');
-Route::get('/tasks/{id}/edit', function() { return view('tasks.edit'); })->name('tasks.showEdit');
-Route::post('/categories/{id}/tasks/create', function() { echo "タスク作成"; })->name('tasks.create');
-Route::post('/tasks/{id}/update', function($id) { echo "タスク更新 (id: {$id})"; })->name('tasks.update');
-Route::post('/tasks/{id}/delete', function($id) { echo "タスク削除 (id: {$id})"; })->name('tasks.delete');
+Route::get("/", [TaskController::class, "showIndexView"])->name("tasks.showIndex");
+Route::get("/categories/create", [CategoryController::class, "showCreateView"])->name("categories.showCreate");
+Route::get("/categories/{id}/edit", [CategoryController::class, "showEditView"])->name("categories.showEdit");
+Route::post("/categories/create", [CategoryController::class, "create"])->name("categories.create");
+Route::post("/categories/{id}/update", [CategoryController::class, "update"])->name("categories.update");
+Route::post("/categories/{id}/delete", [CategoryController::class, "delete"])->name("categories.delete");
+Route::get("/categories/{id}/tasks/create", [TaskController::class, "showCreateView"])->name("tasks.showCreate");
+Route::get("/tasks/{id}/edit", [TaskController::class, "showEditView"])->name("tasks.showEdit");
+Route::post("/categories/{id}/tasks/create", [TaskController::class, "create"])->name("tasks.create");
+Route::post("/tasks/{id}/update", [TaskController::class, "update"])->name("tasks.update");
+Route::post("/tasks/{id}/delete", [TaskController::class, "delete"])->name("tasks.delete");
