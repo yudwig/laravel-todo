aaaa68f02de35082ee9c02c71f6db5de7c73ed80
yudwig <yudwig.dev@gmail.com>
2021-01-07 14:10:42 +0900

    Add controllers.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function showCreateView() {}
+    public function showEditView() {}
+    public function create() {}
+    public function update() {}
+    public function delete() {}
+}
---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function showIndexView() {}
+    public function showCreateView() {}
+    public function showEditView($id) {}
+    public function create() {}
+    public function update($id) {}
+    public function delete($id) {}
+}
