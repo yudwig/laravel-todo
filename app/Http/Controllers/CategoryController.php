56a76ce421646861e4cbab5f3d271ffdd99f25f8
yudwig <yudwig.dev@gmail.com>
2021-01-09 23:23:54 +0900

    Modify category controller.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,12 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function showCreateView() {}
-    public function showEditView() {}
-    public function create() {}
-    public function update() {}
-    public function delete() {}
+    public function showCreateView()
+    {
+        return view("categories.create");
+    }
+
+    public function showEditView($categoryId)
+    {
+        return view("categories.edit", [
+            "category" => Category::where("id", $categoryId)->first()
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        Category::create([
+            "title" => $request->input("title")
+        ]);
+        return redirect(route("tasks.showIndex"));
+    }
+
+    public function update(Request $request, $categoryId)
+    {
+        $category = Category::where("id", $categoryId)->first();
+        $category->fill([
+            "title" => $request->input("title")
+        ]);
+        $category->save();
+        return redirect(route("tasks.showIndex"));
+    }
+
+    public function delete($categoryId)
+    {
+        $category = Category::where("id", $categoryId)->first();
+        $category->delete();
+        return redirect(route("tasks.showIndex"));
+    }
 }
--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -11,56 +11,54 @@ class CategoryTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $seed = true;
+
     public function testShowCreate()
     {
-        $this->get(route('categories.showCreate'))
-            ->assertViewIs('categories.create');
+        $this->get(route("categories.showCreate"))
+            ->assertViewIs("categories.create");
     }
 
     public function testShowEdit()
     {
-        $this->seed();
-        $this->get(route('categories.showEdit', ['id' => 1]))
-            ->assertViewIs('categories.edit')
-            ->assertViewHas('category');
+        $this->get(route("categories.showEdit", ["id" => 1]))
+            ->assertViewIs("categories.edit")
+            ->assertViewHas("category");
     }
 
     public function testCreate()
     {
-        $this->seed();
-        $truth = [
-            'title' => 'TestCreate'
+        $input = [
+            "title" => "TestCreate"
         ];
-        $this->post(route('categories.create'), [
-            'title' => $truth['title']
+        $this->post(route("categories.create"), [
+            "title" => $input["title"]
         ])
-            ->assertRedirect(route('tasks.showIndex'));
-        $latest = Category::orderBy('id', 'DESC')->first();
-        $this->assertEquals($truth['title'], $latest['title']);
+            ->assertRedirect(route("tasks.showIndex"));
+        $latest = Category::orderBy("id", "DESC")->first();
+        $this->assertEquals($input["title"], $latest["title"]);
     }
 
     public function testUpdate()
     {
-        $this->seed();
-        $id = 1;
-        $truth = [
-            'title' => 'TestUpdate'
+        $categoryId = 1;
+        $input = [
+            "title" => "TestUpdate"
         ];
-        $this->post(route('categories.update', ['id' => $id]), [
-            'title' => $truth['title']
+        $this->post(route("categories.update", ["id" => $categoryId]), [
+            "title" => $input["title"]
         ])
-            ->assertRedirect(route('tasks.showIndex'));
-        $after = Category::where('id', $id)->first();
-        $this->assertEquals($truth['title'], $after['title']);
+            ->assertRedirect(route("tasks.showIndex"));
+        $after = Category::where("id", $categoryId)->first();
+        $this->assertEquals($input["title"], $after["title"]);
     }
 
     public function testDelete()
     {
-        $this->seed();
-        $id = 1;
-        $task = Category::where('id', $id)->first();
-        $this->post(route('categories.delete', ['id' => $id]))
-            ->assertRedirect(route('tasks.showIndex'));
-        $this->assertDeleted($task);
+        $categoryId = 1;
+        $category = Category::where("id", $categoryId)->first();
+        $this->post(route("categories.delete", ["id" => $categoryId]))
+            ->assertRedirect(route("tasks.showIndex"));
+        $this->assertDeleted($category);
     }
 }
